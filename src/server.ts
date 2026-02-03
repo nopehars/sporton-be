5d802e9a3d1e5030ec0a672c2e63812ed4afe1f0
nopehars <novaharsanto@gmail.com>
2026-02-03 17:09:38 +0700

    commit session-6

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+// // debugging sementara
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// console.log('--- DEBUGGING ENV ---');
+// console.log('MONGO_URI terdeteksi:', process.env.MONGO_URI ? 'YA (String ditemukan)' : 'TIDAK (Undefined)');
+// console.log('---------------------');
+// // end of debugging sementara
+
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config();
+
+const PORT = process.env.PORT || '5001';
+const MONGO_URI = process.env.MONGO_URI || 'no mongo uri';
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
